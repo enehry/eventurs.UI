77d6910af02c1b06caf430c05c68c0a463252517
nehry <enehry@gmail.com>
2023-05-13 22:29:10 +0800

    fix eslint error Parsing error: The keyword 'import' is reserved

--- src/components/DefaultButton.vue
+++ src/components/DefaultButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="px-4 py-2 rounded-md w-full bg-primary text-whiteText font-semibold text-lg md:text-xl tracking-wide"
+    class="px-4 py-2 rounded-md w-full bg-primary text-whiteText font-medium text-lg md:text-xl tracking-wide"
   >
     <slot></slot>
   </button>
