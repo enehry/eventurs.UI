3aeec245d1dcc7e0876228bc85b5a5a72c45c850
NelsonRyan-06 <nelsonarabit00@gmail.com>
2023-05-16 14:03:58 +0800

    Login and Register View validation

--- src/components/DefaultInput.vue
+++ src/components/DefaultInput.vue
@@ -1,14 +1,36 @@
 <template>
-  <input :type="props.type" :placeholder="props.placeholder" class="login-input" />
+  <input
+    :name="props.name"
+    :type="props.type"
+    :placeholder="props.placeholder"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="login-input"
+    :class="props.errorBorder"
+  />
 </template>
 
 <script setup>
+import { defineProps, defineEmits } from 'vue'
+defineEmits(['update:modelValue'])
 const props = defineProps({
+  name: {
+    type: String,
+    default: ''
+  },
   type: {
     type: String,
     default: 'text'
   },
   placeholder: {
+    type: String,
+    default: ''
+  },
+  modelValue: {
+    type: String,
+    default: ''
+  },
+  errorBorder: {
     type: String,
     default: ''
   }
